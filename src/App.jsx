a10c9c8e5f6e9bc9660723e14fc298c9fffb0bde
upperyorkwildlife-ai <upperyorkwildlife@gmail.com>
2025-12-07 11:11:16 -0500

    Remove lead capture popup for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import LeadPopup from './components/LeadPopup'
 import Home from './pages/Home'
 import Services from './pages/Services'
 import About from './pages/About'
